2e9d19266caa37f46ddf3b61ba20e5ecceaa3cbe
Marc Garrido <91668406+Marukuz@users.noreply.github.com>
2023-04-13 18:03:42 +0200

    Initial commit

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="{{asset('assets/css/tareas.css')}}">
-    <title>Empresa</title>
+    <link rel="stylesheet" href="{{ asset('css/tareas.css') }}">
+    <title>VisualSeyra</title>
 </head>
 <header>
 <nav class="navbar navbar-expand-lg bg-secondary">
